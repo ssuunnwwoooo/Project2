7774847d8714498b7337c68f600f714e5c204eb8
sunwoo <133613205+ssuunnwwoooo@users.noreply.github.com>
2023-05-17 16:41:13 +0900

    230517
    넘어렵다

---
+++ js/main01.js
@@ -0,0 +1,11 @@
+$(function () {
+    $('.mainSlide').slick({
+        //arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        pauseOnHover: false,
+    });
+})
+
+/* autopaly는 슬릭에서 자체개발한 요소 */
